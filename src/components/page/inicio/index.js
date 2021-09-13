780ff6c2ee0291826e67375b14fbdded60bdf972
DESKTOP-LO51G0C\sergi <verasergio700@gmail.com>
2021-09-13 11:47:08 -0500

    Estilos

--- src/components/page/inicio/index.js
+++ src/components/page/inicio/index.js
@@ -5,12 +5,9 @@ import { Link } from "react-router-dom";
 export default function Inicio() {
     return (
         <div className="inicio">   
-            <Link to="/">
-            <h1>home</h1>
-            </Link>
-            <Link to="/productos">
-            <h1>Productos</h1>
-            </Link>
+
+            <h1>Lo mas vendido</h1>
+
             
         </div>
     )
